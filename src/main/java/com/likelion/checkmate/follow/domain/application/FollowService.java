13d289b36f64973a39b32dd7776b5ccdb11aaf81
EunSeo35 <pes1365@naver.com>
2023-08-08 20:11:50 +0900

    ✨ feat : 언팔로우 기능 구현

---
+++ src/main/java/com/likelion/checkmate/follow/domain/application/FollowService.java
@@ -0,0 +1,24 @@
+package com.likelion.checkmate.follow.domain.application;
+
+
+import com.likelion.checkmate.follow.domain.entity.Follow;
+import com.likelion.checkmate.follow.domain.repository.FollowRepository;
+import com.likelion.checkmate.post.domain.entity.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class FollowService {
+
+    private final FollowRepository followRepository;
+
+    @Transactional
+    public void deleteFollow (Long followId, Long userId) {
+        Follow follow = followRepository.findByIdAndUserId(followId, userId);
+        followRepository.deleteById(followId);
+    }
+
+
+}
--- src/main/java/com/likelion/checkmate/follow/domain/repository/FollowRepository.java
+++ src/main/java/com/likelion/checkmate/follow/domain/repository/FollowRepository.java
@@ -1,7 +1,9 @@
 package com.likelion.checkmate.follow.domain.repository;
 
 import com.likelion.checkmate.follow.domain.entity.Follow;
+import com.likelion.checkmate.post.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    Follow findByIdAndUserId(Long followId, Long userId);
 }
